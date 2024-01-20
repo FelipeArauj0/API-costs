890db4970e129a628cb7982969b7147c0d0f7525
Felipe Araujo <fearaujo871@gmail.com>
2024-01-19 23:22:45 -0300

    correção config

--- src/corsConfig.js
+++ src/corsConfig.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',

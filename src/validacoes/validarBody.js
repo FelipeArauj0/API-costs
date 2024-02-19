c6d62eddff86a70cbd16a5b463ee447d09b379e1
Felipe Araujo <fearaujo871@gmail.com>
2024-02-19 13:08:45 -0300

    correção validarBody

--- src/validacoes/validarBody.js
+++ src/validacoes/validarBody.js
@@ -3,6 +3,7 @@ const joi = require('joi')
 const validarBody = schema => async (req,res,next)=>{
     try {
         await schema.validateAsync(req.body)
+        next()
     } catch (error) {
         return res.status(400).json({menssagem: error.message})
     }

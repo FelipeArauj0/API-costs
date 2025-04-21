daa74079ff89de26c8a162bfd2a688f2b198e620
unknown <fearaujo871@gmail.com>
2025-04-21 15:04:47 -0300

    conexao com banco de dados neon

--- src/conexaoDB.js
+++ src/conexaoDB.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host : process.env.DB_HOST,
-      port : 5432,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
-    }
-  });
 
-  module.exports = knex
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // importante pro Neon
+})
+
+module.exports = knex
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//       host : process.env.DB_HOST,
+//       port : 5432,
+//       user : process.env.DB_USER,
+//       password : process.env.DB_PASS,
+//       database : process.env.DB_NAME
+//     }
+//   });
+
+
+
+  // module.exports = knex
+
+  
